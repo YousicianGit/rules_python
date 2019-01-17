0daf9557f705ad06517193082185d14d52eca3df
Matthieu <matthieu@yousician.com>
2019-01-17 17:18:34 +0200

    Adding static directory for pip builds

--- rules_python/piptool.py
+++ rules_python/piptool.py
@@ -154,7 +154,7 @@ def main():
   args = parser.parse_args()
 
   # https://github.com/pypa/pip/blob/9.0.1/pip/__init__.py#L209
-  if pip_main(["wheel", "-w", args.directory, "-r", args.input]):
+  if pip_main(["wheel", "-b", "/tmp/pip-build", "-w", args.directory, "-r", args.input]):
     sys.exit(1)
 
   # Enumerate the .whl files we downloaded.
